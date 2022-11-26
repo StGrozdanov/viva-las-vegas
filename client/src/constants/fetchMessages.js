8f899bb32a83409507ffdaa45c9850f4e90b695e
Stoyan Grozdanov <st.grozdanov@abv.bg>
2022-11-26 19:56:13 +0200

    Delete client directory

--- client/src/constants/fetchMessages.js
+++
@@ -1,2 +0,0 @@
-export const COULD_NOT_FETCH_USERS = 'Something went wrong. Could not fetch users.';
-export const COULD_NOT_EVALUATE_REGISTRATION_STATE = 'Could not evaluate registration state for this user.';
--- client/src/constants/secondStepFeedbackFormProperties.js
+++
@@ -1,9 +0,0 @@
-const secondStepFeedbackFormProperties = {
-    headingText: 'You’re ready to play!',
-    text: 'In order to play with real money, you will need to provide more information. You can do so now if you wish or skip and do it at a later time.',
-    firstButtonText: 'Continue Registration',
-    secondButtonText: 'Skip',
-    firstHref: 'http://casino.com/',
-}
-
-export default secondStepFeedbackFormProperties;
